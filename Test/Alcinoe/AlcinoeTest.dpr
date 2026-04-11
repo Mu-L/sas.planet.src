14c5ec54706c37ba2d86edfa6faf9d337688120c
zed <starmen@tut.by>
2026-04-11 18:29:13 +0300

    Проект для теста компиляции юнитов Alcinoe

---
+++ Test/Alcinoe/AlcinoeTest.dpr
@@ -0,0 +1,17 @@
+program AlcinoeTest;
+
+{$APPTYPE CONSOLE}
+
+uses
+  Alcinoe.HTTP.Client,
+  Alcinoe.HTTP.Client.WinInet,
+  Alcinoe.Localization,
+  Alcinoe.StringList,
+  Alcinoe.StringUtils,
+  Alcinoe.XMLDoc,
+  u_AnsiStr in '..\..\Src\u_AnsiStr.pas';
+
+begin
+  Writeln('Press ENTER to exit...');
+  Readln;
+end.
