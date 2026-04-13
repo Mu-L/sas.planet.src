780e5289f08a92cba4937869c6da80d1614da678
zed <starmen@tut.by>
2026-04-13 13:49:11 +0300

    Забытая шапка, плюс мелкий фикс

--- Src/MarksDB/GUI/u_MarksExplorerView.pas
+++ Src/MarksDB/GUI/u_MarksExplorerView.pas
@@ -1,3 +1,24 @@
+{******************************************************************************}
+{* This file is part of SAS.Planet project.                                   *}
+{*                                                                            *}
+{* Copyright (C) 2007-Present, SAS.Planet development team.                   *}
+{*                                                                            *}
+{* SAS.Planet is free software: you can redistribute it and/or modify         *}
+{* it under the terms of the GNU General Public License as published by       *}
+{* the Free Software Foundation, either version 3 of the License, or          *}
+{* (at your option) any later version.                                        *}
+{*                                                                            *}
+{* SAS.Planet is distributed in the hope that it will be useful,              *}
+{* but WITHOUT ANY WARRANTY; without even the implied warranty of             *}
+{* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the               *}
+{* GNU General Public License for more details.                               *}
+{*                                                                            *}
+{* You should have received a copy of the GNU General Public License          *}
+{* along with SAS.Planet. If not, see <http://www.gnu.org/licenses/>.         *}
+{*                                                                            *}
+{* https://github.com/sasgis/sas.planet.src                                   *}
+{******************************************************************************}
+
 unit u_MarksExplorerView;
 
 interface
@@ -1020,9 +1041,9 @@ begin
         if ASelected.UID = GetNodeUniqueID(VNodeData.Category.MarkCategory) then begin
           FCategoryTree.ClearSelection;
           FCategoryTree.Selected[VNode] := True;
-          VSelectedFound := True;
         end;
       end;
+      VSelectedFound := True;
     end;
 
     if VSelectedFound and (VExpandedIndex >= VExpandedLen) then begin
